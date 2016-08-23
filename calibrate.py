86013796912e4353c2997420e9596d1c1d9273cb
VikrantKamble <vikrant.kamble1990@gmail.com>
2016-08-23 16:36:38 -0600

    Function defination in new file

--- calibrate.py
+++ calibrate.py
@@ -1,36 +1,56 @@
 from __future__  import division
+from func_defs import compute_alpha
 import fitsio
 import numpy as np
 
 # AIM: TO STUDY DEVIATIONS FROM POWER LAW - ESTIMATE CALIBRATION SYSTEMATICS
 
 # DEFINE MASTER COMPOSITE AND NORMALIZE IT CORRECTLY
+m_comp = fitsio.read('../Composites/comp_V0_44_21_equalobj/comp_V0_44_21_equalobj.fits')[16]
+m_ivar = fitsio.read('../Composites/comp_V0_44_21_equalobj/comp_V0_44_21_equalobj.fits', ext=1)[16]
 
 # Use the range where there are no emission lines
-ratio_range = [1300, 1400]
+ratio_range = [1250, 1480]
 
+
+alpha, amp = tb['ALPHA_V0'], tb['AMP_V0']
 z = tb[drq_z]
-lowz_ind = np.where(z < 2.1)[0]
+
+# Get low-z stuff
+lowz_ind = np.where((z > 1.6) & (z < 2.3) & (alpha > -2) & (alpha < -1))[0]
 lowz_spec, lowz_ivar, lowz = spec[lowz_ind], ivar[lowz_ind], z[lowz_ind]
+lowz_amp, lowz_alpha = amp[lowz_ind], alpha[lowz_ind]
 
 # The range over which to normalize 
-# HAS TO BE PUT BY HAND TO CHOOSE EMISSION FREE RANGE
-wlInd = np.where((wl > 2100) & (wl < 2140))[0]
+# # HAS TO BE PUT BY HAND TO CHOOSE EMISSION FREE RANGE
+# wlInd = np.where((wl > 2100) & (wl < 2140))[0]
 
-# Normalize the spectra and the variance 
-scale   = np.median(lowz_spec[:,wlInd], axis=1)
-lowz_spec  = lowz_spec / scale[:, None]
-lowz_ivar  = lowz_ivar * (scale[:,None])**2
+# # Normalize the spectra and the variance 
+# scale   = np.median(lowz_spec[:,wlInd], axis=1)
+# lowz_spec  = lowz_spec / scale[:, None]
+# lowz_ivar  = lowz_ivar * (scale[:,None])**2
+
+# Normalize in amplitude
+lowz_spec = lowz_spec / lowz_amp[:, None]
+lowz_ivar = lowz_ivar * (lowz_amp[:, None])**2
+
+# Normalize in alpha
+lowz_spec = lowz_spec * (wl/1450)**(-1.5 - lowz_alpha[:,None])
+
+# Normalize master composite
+m_param = compute_alpha(wl, m_comp, m_ivar, [[1280,1290],[1320,1330],[1345,1360],[1440,1480]], 3)
+m_comp = m_comp * (wl/1450)**(-1.5 - m_param[1])
+
 
 lam_ind = np.where((wl > ratio_range[0]) & (wl <= ratio_range[1]))[0]
 
-vals, weights = lowz_spec[:,lam_ind] / master[lam_ind], lowz_ivar[:,lam_ind]
+vals, weights = lowz_spec[:,lam_ind] / m_comp[lam_ind], lowz_ivar[:,lam_ind]
 
 # Mapping rest-frame pixel to observer frame
 lam_obs = np.array(np.mat(wl[lam_ind]).T * np.mat(1+lowz)).T
 
 mylam, myval, myivar = np.ravel(lam_obs), np.ravel(vals), np.ravel(weights)
-divs = np.linspace(min(mylam), max(mylam), 100)
+divs = np.arange(min(mylam), max(mylam), 4)
 
 Z_val = (divs[1:] + divs[:-1]) / 2
 T_val, S_val = np.zeros((2, len(Z_val)))
@@ -38,9 +58,9 @@ T_val, S_val = np.zeros((2, len(Z_val)))
 # Bin values using weighted average statistic
 for i in range(len(divs)-1):
 	ind = np.where((mylam > divs[i]) & (mylam <= divs[i+1]))
-	zeros_ind = np.where(myivar[ind] > 0)[0]
+	zeros_ind = np.where((myivar[ind] > 0))[0]
 	if (len(zeros_ind) > 0):
 		T_val[i] = np.median(myval[ind][zeros_ind])
-		S_val[i] = np.std(myval[ind][zeros_ind])/len(zeros_ind)
+		S_val[i] = np.std(myval[ind][zeros_ind]) #/len(zeros_ind)
 		#T_val[i] = np.average(myval[ind][zeros_ind], weights=myivar[ind][zeros_ind])
 		
--- estimate.py
+++ estimate.py
@@ -3,6 +3,7 @@ from scipy.optimize import curve_fit
 from astropy.convolution import convolve, Box1DKernel
 from scipy.optimize import minimize
 from scipy.integrate import quad
+from func_defs import *
 import numpy as np
 import fitsio
 import timeit
@@ -26,128 +27,7 @@ per_value = float(X['per_value'])
 
 nObj = len(z)
 
-# HELPER FUNCTION DEFINATIONS 
-def myfunct(wave, a , b):return a*(wave/1450)**b
-
-# Linear model for local continuum
-def lin_funct(wave, a , b):return a*wave + b
-
-# Double gaussian to model the CIV line
-def gauss_funct(x, *theta):
-	A1, A2, S1, S2, C1, C2,  = theta
-	return  A1*np.exp(-(x - C1)**2/S1**2) + A2*np.exp(-(x - C2)**2/S2**2)
-
-# Helper function for calculating EW of CIV
-def f_ratio(x, *theta):
-	temp = theta[2:8]
-	return gauss_funct(x, *temp)/lin_funct(x,theta[0],theta[1]) 
-# -----------------------------------------------------------------------------------
-
-# MAIN FUNCTION DEFINATIONS
-def compute_alpha(wl, spec, ivar, wav_range, per_value, plotit=0):
-		wavelength, spectra, invar = np.array([]), np.array([]), np.array([])
-
-		for j in range(len(wav_range)):
-			temp = np.where((wl > wav_range[j][0]) & (wl < wav_range[j][1]))[0]
-			tempspec, tempivar  = spec[temp], ivar[temp]
-			
-			#Mask out metal absorption lines
-			cut = np.percentile(tempspec, per_value)
-			blah = np.where((tempspec > cut) & (tempivar > 0))[0]
-			wave = wl[temp][blah]
-
-			# Create vectors for model fitting
-			wavelength = np.concatenate((wavelength, wave))
-			spectra = np.concatenate((spectra, tempspec[blah]))
-			invar = np.concatenate((invar, tempivar[blah]))
-		
-		try:
-			popt, pcov = curve_fit(myfunct, wavelength, spectra, sigma=1.0/np.sqrt(invar))
-		except (RuntimeError, TypeError):
-			return 0, 0, -1, 0
-		else:
-			AMP, ALPHA = popt[0], popt[1]
-			CHISQ = np.sum(invar * (spectra - myfunct(wavelength, popt[0], popt[1]))**2)
-			# DOF = N - n  , n = 2
-			DOF = len(spectra) - 2
-			if (plotit == 1):
-				plot(wl, spec, linewidth=0.4, alpha=0.7, color='black')
-				plot(wl, myfunct(wl, popt[0], popt[1]), color='red')
-			return AMP, ALPHA, CHISQ, DOF		
-
-def compute_ew(wl, spec, ivar, lin_range, gauss_range, per_value, plotit=0):
-	wavelength, spectra, invar = np.array([]), np.array([]), np.array([])
-	for j in range(len(lin_range)):
-		temp = np.where((wl > lin_range[j][0]) & (wl < lin_range[j][1]))[0]
-		tempspec, tempivar  = spec[temp], ivar[temp]
-		
-		#Mask out metal absorption lines
-		cut = np.percentile(tempspec, per_value)
-		blah = np.where((tempspec > cut) & (tempivar > 0))[0]
-		wave = wl[temp][blah]
-
-		wavelength = np.concatenate((wavelength, wave))
-		spectra = np.concatenate((spectra, tempspec[blah]))
-		invar = np.concatenate((invar, tempivar[blah]))
-
-	try:
-		# Fit a linear local continuum
-		popt = curve_fit(lin_funct, wavelength, spectra, sigma=1.0/np.sqrt(invar))[0]
-
-		ind = np.where((wl > gauss_range[0]) & (wl < gauss_range[1]))[0]
-		
-		mywl, myivar = wl[ind], ivar[ind]
-		myspec = spec[ind] - lin_funct(mywl, popt[0], popt[1])
-
-		s = np.where(myivar > 0)[0]
 	
-		# Smoothing to remove narrow absoprtion troughs, more than 3 sigma away 
-		smooth = convolve(myspec[s], Box1DKernel(20))
-		t = s[np.where(np.sqrt(myivar[s])*(smooth - myspec[s]) < 3)[0]]
-
-		W, S, I  = mywl[t], myspec[t], myivar[t]
-	
-		# Lambda function to specify the minimzation format
-		err = lambda p: np.sum(I*(gauss_funct(W,*p) - S)**2) 
-
-		# Initial guess for the parameters
-		A1, A2 = max(smooth), max(smooth)/3.0
-		C1 = C2 = mywl[s[np.argmax(smooth)]]
-		S1 = np.sqrt(np.sum((mywl[s]-C1)**2 * smooth)/np.sum(smooth))
-		S2 = S1*2.0
-
-		# Initial guess for the optimiztion module
-		p_init = [A1,A2,S1,S2,C1,C2]
-
-		# Bounded optimization - Removes contimation from nearby lines especially He II
-		bounds=[(0,None),(0,None),(5,30),(0,50),(1520,1580),(1520,1580)]
-
-		chi_init = np.inf
-
-		# k indicates the number of recursive iterations to perform
-		for k in range(3):
-			p_opt = minimize(err, p_init, bounds = bounds,	method="L-BFGS-B").x
-
-			if (err(p_opt) < chi_init):
-				chi_init = err(p_opt)
-				dummy = np.sqrt(I)*(S - gauss_funct(W,*p_opt)) > -2
-				W, S, I = W[dummy], S[dummy], I[dummy]
-			else: break
-
-	except (RuntimeError, ValueError, TypeError): 
-		return -1, np.zeros(2), np.zeros(6), 0, 0
-	else:
-		lin_param, gauss_param = popt, p_opt
-		chisq, dof = err(p_opt), len(S) - 6
-
-		ew = quad(f_ratio, 1460, 1640, args = tuple([popt[0], popt[1]] + list(p_opt)))[0]
-
-		if (plotit == 1):
-			plot(wl, spec, linewidth=0.4, color='k')
-			plot(wl, lin_funct(wl, popt[0], popt[1]) + gauss_funct(wl, *p_opt))
-		return ew, lin_param, gauss_param, chisq, dof
-
-#------------------------------------------------------------------------------------	
 # 1. Calculation of the spectral index
 if X['alpha_flag'] == 'Yes':
 	ver = X['alpha_ver']
